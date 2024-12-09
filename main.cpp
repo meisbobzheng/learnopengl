1761b38bc43f716da7675c2e70fe53cbfcbd0a83
Bob Zheng <bob.zheng6042@gmail.com>
2024-12-09 10:31:10 -0500

    nullptr

--- main.cpp
+++ main.cpp
@@ -9,6 +9,12 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height){
     std::cout << "width" + std::to_string(width) + " height" + std::to_string(height) << std::endl;
 }
 
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -16,8 +22,8 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -37,6 +43,14 @@ int main() {
 
     while(!glfwWindowShouldClose(window))
     {
+        // input
+        processInput(window);
+
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // check and call events and swap buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
